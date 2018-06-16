344c666dd804aa8199bf47506c45740957cb0a20
ValarDragon <dojha12@gmail.com>
2018-06-15 19:24:29 -0700

    Add license notice, handle errors, abstract constants

--- hkdfchacha20poly1305/hkdfchachapoly.go
+++ hkdfchacha20poly1305/hkdfchachapoly.go
@@ -1,17 +1,19 @@
 package hkdfchacha20poly1305
 
+// This package creates an AEAD using hkdf, chacha20, and poly1305
+// When sealing and opening, the hkdf is used to obtain the nonce and subkey for
+// chacha20. Other than the change for the how the subkey and nonce for chacha
+// are obtained, this is the same as chacha20poly1305
+
 import (
 	"crypto/cipher"
 	"crypto/sha256"
 	"errors"
-	"fmt"
 	"io"
 
 	"golang.org/x/crypto/chacha20poly1305"
 	"golang.org/x/crypto/hkdf"
 )
-
-var sigma = [4]uint32{0x61707865, 0x3320646e, 0x79622d32, 0x6b206574}
 
 type hkdfchacha20poly1305 struct {
 	key [KeySize]byte
@@ -22,6 +24,13 @@ const (
 	KeySize = 32
 	// NonceSize is the size of the nonce used with this AEAD, in bytes.
 	NonceSize = 24
+	// TagSize is the size added from poly1305
+	TagSize = 16
+	// MaxPlaintextSize is the max size that can be passed into a single call of Seal
+	MaxPlaintextSize = (1 << 38) - 64
+	// MaxCiphertextSize is the max size that can be passed into a single call of Open,
+	// this differs from plaintext size due to the tag
+	MaxCiphertextSize = (1 << 38) - 48
 	// HkdfInfo is the parameter used internally for Hkdf's info parameter.
 	HkdfInfo = "TENDERMINT_SECRET_CONNECTION_FRAME_KEY_DERIVE"
 )
@@ -41,7 +50,7 @@ func (c *hkdfchacha20poly1305) NonceSize() int {
 }
 
 func (c *hkdfchacha20poly1305) Overhead() int {
-	return 16
+	return TagSize
 }
 
 func (c *hkdfchacha20poly1305) Seal(dst, nonce, plaintext, additionalData []byte) []byte {
@@ -49,39 +58,49 @@ func (c *hkdfchacha20poly1305) Seal(dst, nonce, plaintext, additionalData []byte
 		panic("hkdfchacha20poly1305: bad nonce length passed to Seal")
 	}
 
-	if uint64(len(plaintext)) > (1<<38)-64 {
+	if uint64(len(plaintext)) > MaxPlaintextSize {
 		panic("hkdfchacha20poly1305: plaintext too large")
 	}
 
-	var subKey [KeySize]byte
-	var chachaNonce [chacha20poly1305.NonceSize]byte
+	subKey, chachaNonce := getSubkeyAndChachaNonceFromHkdf(&c.key, &nonce)
 
-	hash := sha256.New
-	hkdf := hkdf.New(hash, c.key[:], nonce, []byte(HkdfInfo))
-	io.ReadFull(hkdf, subKey[:])
-	io.ReadFull(hkdf, chachaNonce[:])
-
-	chacha20poly1305, _ := chacha20poly1305.New(subKey[:])
+	chacha20poly1305, err := chacha20poly1305.New(subKey[:])
+	if err != nil {
+		panic("hkdfchacha20poly1305: failed to initialize chacha20poly1305")
+	}
 
 	return chacha20poly1305.Seal(dst, chachaNonce[:], plaintext, additionalData)
 }
 
 func (c *hkdfchacha20poly1305) Open(dst, nonce, ciphertext, additionalData []byte) ([]byte, error) {
 	if len(nonce) != NonceSize {
-		return nil, fmt.Errorf("hkdfchacha20poly1305: bad nonce length passed to Open")
+		return nil, errors.New("hkdfchacha20poly1305: bad nonce length passed to Open")
 	}
-	if uint64(len(ciphertext)) > (1<<38)-48 {
-		return nil, fmt.Errorf("hkdfchacha20poly1305: ciphertext too large")
+	if uint64(len(ciphertext)) > MaxCiphertextSize {
+		return nil, errors.New("hkdfchacha20poly1305: ciphertext too large")
 	}
-	var subKey [KeySize]byte
-	var chachaNonce [chacha20poly1305.NonceSize]byte
 
-	hash := sha256.New
-	hkdf := hkdf.New(hash, c.key[:], nonce, []byte(HkdfInfo))
-	io.ReadFull(hkdf, subKey[:])
-	io.ReadFull(hkdf, chachaNonce[:])
+	subKey, chachaNonce := getSubkeyAndChachaNonceFromHkdf(&c.key, &nonce)
 
-	chacha20poly1305, _ := chacha20poly1305.New(subKey[:])
+	chacha20poly1305, err := chacha20poly1305.New(subKey[:])
+	if err != nil {
+		panic("hkdfchacha20poly1305: failed to initialize chacha20poly1305")
+	}
 
 	return chacha20poly1305.Open(dst, chachaNonce[:], ciphertext, additionalData)
+}
+
+func getSubkeyAndChachaNonceFromHkdf(cKey *[32]byte, nonce *[]byte) (
+	subKey [KeySize]byte, chachaNonce [chacha20poly1305.NonceSize]byte) {
+	hash := sha256.New
+	hkdf := hkdf.New(hash, (*cKey)[:], *nonce, []byte(HkdfInfo))
+	_, err := io.ReadFull(hkdf, subKey[:])
+	if err != nil {
+		panic("hkdfchacha20poly1305: failed to read subkey from hkdf")
+	}
+	_, err = io.ReadFull(hkdf, chachaNonce[:])
+	if err != nil {
+		panic("hkdfchacha20poly1305: failed to read chachaNonce from hkdf")
+	}
+	return
 }
